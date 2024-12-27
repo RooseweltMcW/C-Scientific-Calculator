d60be246552d29b729b38ded9073024ccf27a399
RooseweltMcW <austin.roosewelt@multicorewareinc.com>
2024-12-27 14:17:44 +0530

    Add : implemented all the functions with respective logics without any built in function

--- Calculator.cpp
+++ Calculator.cpp
@@ -29,16 +29,53 @@ template <typename T> class ScientificCalculator
             return num1/num2;
         }
         T power(T base,T exp){
-            return pow(base,exp);
+            // return pow(base,exp);
+            if(exp == 0 ) return 1;
+            T result=1;
+            bool negativeExp=false;
+            if(exp<0){
+                negativeExp=true;
+                exp=-exp;
+            }
+            while(exp>0){
+                if(static_cast<int>(exp)%2==1){
+                    result *=base;
+                }
+                base *=base;
+                exp/=2;
+            }
+            if(negativeExp){
+                return 1/result;
+            }
+            return result;
         }
         T squareRoot(T value){
             if(value<0){
                 throw invalid_argument("square root of negative number is not defined");
             }
-            return sqrt(value);
+            // return sqrt(value);
+            T guess = value / 2;
+            T tolerance = 0.000001;
+            while (true) {
+                T newGuess = (guess + value / guess) / 2;
+                if (abs(guess - newGuess) < tolerance) {
+                    break;
+                }
+                guess = newGuess;
+            }
+            return guess;   
+
         }
         T exponential(T value) {
-            return exp(value); 
+            // return exp(value); 
+            T result = 1;
+            T term = 1;
+            for (int i = 1; i < 20; ++i) {
+                term *= value / i;
+                result += term;
+            }
+            return result;
+
         }
         T fraction(T numerator,T denominator){
             if(denominator==0){
@@ -50,16 +87,55 @@ template <typename T> class ScientificCalculator
             if(num1 <= 0){
                 throw invalid_argument("Logarithm of zero or negative is not defined");
             }
-            return log(num1);
+            // return log(num1);
+            T x = num1 - 1;
+            T result = 0;
+            T term = x;
+            for (int i = 1; i < 20; ++i) {
+                if (i % 2 == 1) {
+                    result += term / i;
+                } else {
+                    result -= term / i;
+                }
+                term *= x;
+            }
+            return result;
+
         }
         T sine(T angle){
-            return sin(angle);
+            // return sin(angle);
+            T result = 0;
+            T term = angle;
+            int sign = 1;
+            for (int i = 1; i < 10; i += 2) {
+                result += sign * term;
+                term *= angle * angle / ((i + 1) * (i + 2));
+                sign = -sign;
+            }
+return result;
+
         }
         T cosine(T angle){
-            return cos(angle);
+            // return cos(angle);
+            T result = 1;
+            T term = 1;
+            int sign = -1;
+            for (int i = 2; i < 10; i += 2) {
+                term *= angle * angle / ((i - 1) * i);
+                result += sign * term;
+                sign = -sign;
+            }
+            return result;
+
         }
         T tangent(T angle){
-            return tan(angle);
+            // return tan(angle);
+            T sinVal = sine(angle);
+            T cosVal = cosine(angle);
+            if (cosVal == 0) {
+                throw invalid_argument("Tangent of angle with cos(x) = 0 is undefined");
+            }
+            return sinVal / cosVal;
         }
 };
 
